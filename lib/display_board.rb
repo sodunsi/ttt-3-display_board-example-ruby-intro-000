18b7a4f1750c26e0d8b0b89849650da6d762eb37
S.O <shakirat.odunsi@gmail.com>
2018-06-15 11:06:10 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,9 @@
 def display_board
-  puts "A Tic Tac Toe Board"
+  #puts "A Tic Tac Toe Board"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
 end
