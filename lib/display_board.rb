224a17a1a508a8c26db5819c934b345f46439e7f
S.O <shakirat.odunsi@gmail.com>
2018-06-15 11:00:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,9 @@
 def display_board
-  puts "A Tic Tac Toe Board" 
+  puts "A Tic Tac Toe Board"
+  puts "#{board[0]}  | #{board[5]} |#{board[2]}  "
+  puts "-----------"
+  puts "#{board[0]}  | #{board[1]} |#{board[2]}  "
+  puts "-----------"
+  puts "#{board[0]}  | #{board[1]} |#{board[2]}  "
+  puts "#{board[0]}  | #{board[1]} |#{board[2]}  "
 end
